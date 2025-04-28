197b727856d8aede3662d181cfb266d3b56380de
maksimka1234567 <povaliaevmaksim@yandex.ru>
2025-04-28 21:39:09 +0400

    hello

--- main.py
+++ main.py
@@ -13,6 +13,18 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 WAITING_FOR_INPUT = 1
+WAITING_FOR_FIRST_OBJECT = 2
+WAITING_FOR_SECOND_OBJECT = 3
+SERVER_ADDRESS = 'http://geocode-maps.yandex.ru/1.x/?'
+API_KEY = '8013b162-6b42-4997-9691-77b7074026e0'
+
+
+def get_coords(user_input):
+    geocoder_request = f'{SERVER_ADDRESS}apikey={API_KEY}&geocode={user_input}&format=json'
+    response = requests.get(geocoder_request)
+    data = response.json()
+    coordinates = data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+    return coordinates
 
 
 # Обработчик команды /start
@@ -32,6 +44,11 @@ async def start_geocode(update: Update, context: ContextTypes.DEFAULT_TYPE):
     return WAITING_FOR_INPUT
 
 
+async def start_route(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Введите название первого географического объекта:')
+    return WAITING_FOR_FIRST_OBJECT
+
+
 # Обработка ввода пользователя
 async def handle_location(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not update.message:
@@ -41,18 +58,15 @@ async def handle_location(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_input = update.message.text
 
     chat_id = update.message.chat_id
-    server_address = 'http://geocode-maps.yandex.ru/1.x/?'
-    api_key = '8013b162-6b42-4997-9691-77b7074026e0'
 
     try:
-        geocoder_request = f'{server_address}apikey={api_key}&geocode={user_input}&format=json'
-        response = requests.get(geocoder_request)
-        data = response.json()
-        coordinates = data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
-        longitude, latitude = map(float, coordinates.split())
+        longitude, latitude = map(float, get_coords(user_input).split())
+        # Вычисляем bbox для одного объекта (небольшая область вокруг точки)
+        delta = 0.01  # Размер области вокруг точки (широта/долгота)
+        bbox = f"{longitude - delta},{latitude - delta}~{longitude + delta},{latitude + delta}"
         geocoder_request = [
-            f"http://yandex.ru/maps/?ll={longitude},{latitude}&spn=0.01,0.01&l=map&pt={longitude},{latitude},pm2rdm",
-            f"http://static-maps.yandex.ru/1.x/?ll={longitude},{latitude}&spn=0.01,0.01&l=map&pt={longitude},{latitude},pm2rdm"
+            f"http://yandex.ru/maps/?ll={longitude},{latitude}&z=15&l=map&pt={longitude},{latitude},pm2rdm",
+            f"http://static-maps.yandex.ru/1.x/?ll={longitude},{latitude}&bbox={bbox}&l=map&pt={longitude},{latitude},pm2rdm"
         ]
 
         keyboard = [
@@ -63,27 +77,95 @@ async def handle_location(update: Update, context: ContextTypes.DEFAULT_TYPE):
         response1 = requests.get(geocoder_request[1])
         image_data = response1.content
         image = Image.open(BytesIO(image_data))
-        image.save("yandex_map.png")
+        image.save("img.png")
 
-        with open('yandex_map.png', 'rb') as photo:
+        with open("img.png", 'rb') as photo:
             await context.bot.send_photo(
                 chat_id=chat_id,
                 photo=photo,
                 caption=f"Найден объект: {user_input}\nНажмите на кнопку, чтобы открыть карту:",
                 reply_markup=reply_markup
             )
+        return ConversationHandler.END
+
     except Exception as e:
         logger.error(f"Произошла ошибка: {e}")
         await update.message.reply_text('Не удаётся найти данный географический объект.')
+        return WAITING_FOR_INPUT
+
+
+async def handle_first_object(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not update.message:
+        logger.debug("Получено обновление без текстового сообщения")
+        return
+
+    user_input = update.message.text
+    try:
+        longitude, latitude = map(float, get_coords(user_input).split())
+        context.user_data['first_coords'] = (longitude, latitude)
+        await update.message.reply_text('Первый объект найден. Введите название второго географического объекта:')
+        return WAITING_FOR_SECOND_OBJECT
+    except Exception as e:
+        logger.error(f"Произошла ошибка при обработке первого объекта: {e}")
+        await update.message.reply_text('Не удаётся найти данный географический объект. Попробуйте снова.')
+        return WAITING_FOR_FIRST_OBJECT
+
+
+async def handle_second_object(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not update.message:
+        logger.debug("Получено обновление без текстового сообщения")
+        return
+
+    user_input = update.message.text
+    try:
+        longitude, latitude = map(float, get_coords(user_input).split())
+        first_coords = context.user_data.get('first_coords')
+        await update.message.reply_text('Второй объект найден.')
+        await route(update, context, first_coords, (longitude, latitude))
+        return ConversationHandler.END
+    except Exception as e:
+        logger.error(f"Произошла ошибка при обработке второго объекта: {e}")
+        await update.message.reply_text('Не удаётся найти данный географический объект. Попробуйте снова.')
+        return WAITING_FOR_SECOND_OBJECT
+
+
+async def route(update: Update, context: ContextTypes.DEFAULT_TYPE, start, end):
+    response_for_route = (
+        f"https://static-maps.yandex.ru/1.x/?l=map&pl="
+        f"c:ec473fFF,w:5,{start[0]},{start[1]},{end[0]},{end[1]}"
+        f"&pt={start[0]},{start[1]},pm2rdm~{end[0]},{end[1]},pm2rdm"
+    )
+    chat_id = update.message.chat_id
+    response = requests.get(response_for_route)
+    image_data = response.content
+    image = Image.open(BytesIO(image_data))
+    keyboard = [
+        [InlineKeyboardButton("Открыть карту",
+                              url=f"https://yandex.ru/maps/?rtext={start[1]},{start[0]}~{end[1]},{end[0]}")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    image.save("img.png")
+
+    with open("img.png", 'rb') as photo:
+        await context.bot.send_photo(
+            chat_id=chat_id,
+            photo=photo,
+            caption="Маршрут построен\nНажмите на кнопку, чтобы открыть карту:",
+            reply_markup=reply_markup
+        )
     return ConversationHandler.END
 
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('geocode', start_geocode)],
+        entry_points=[CommandHandler('geocode', start_geocode),
+                      CommandHandler('route', start_route)
+                      ],
         states={
-            WAITING_FOR_INPUT: [MessageHandler(filters.ALL, handle_location)],
+            WAITING_FOR_INPUT: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_location)],
+            WAITING_FOR_FIRST_OBJECT: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_first_object)],
+            WAITING_FOR_SECOND_OBJECT: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_second_object)],
         },
         fallbacks=[],
     )
@@ -96,4 +178,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
